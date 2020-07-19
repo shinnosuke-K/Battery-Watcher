f338a31245c5642ed75d6c65946561535d1d5ce0
shinnosuke-K <shin.050195@gmail.com>
2020-07-19 12:55:07 +0900

    add
    出力処理を記述

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
-	"strconv"
+	"os"
 	"strings"
 
 	"gopkg.in/pipe.v2"
@@ -25,7 +26,7 @@ func main() {
 	cmdResults := fmt.Sprintf("%s", byteResults)
 	cmdSlices := strings.Split(strings.ReplaceAll(cmdResults, " ", ""), "\n")[:3]
 
-	capacity := make(map[string]int)
+	capacity := make(map[string]string)
 	for _, cs := range cmdSlices {
 		trimName := strings.TrimLeftFunc(cs, func(r rune) bool {
 			return string(r) != "\""
@@ -42,12 +43,30 @@ func main() {
 		})
 
 		capVol := strings.ReplaceAll(trimVol, "=", "")
-
-		capacity[capName], err = strconv.Atoi(capVol)
-		if err != nil {
-			log.Fatalln(err)
-		}
+		capacity[capName] = capVol
 	}
 
 	fmt.Println(capacity)
+
+	file, err := os.OpenFile("cap.csv", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Turn: MaxCapacity, CurrentCapacity, DesignCapacity
+	csvFile := csv.NewWriter(file)
+	var insertVal []string
+	for k, v := range capacity {
+		fmt.Println(k, v)
+		insertVal = append(insertVal, v)
+	}
+
+	if err := csvFile.Write(insertVal); err != nil {
+		log.Fatalln(err)
+	}
+
+	csvFile.Flush()
+	if err := csvFile.Error(); err != nil {
+		log.Fatalln(err)
+	}
 }
